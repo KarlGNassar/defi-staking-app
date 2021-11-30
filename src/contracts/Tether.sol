f2c4fb76559cf67930a50877d931a3fed4570e73
KarlGNassar <kgnassar01@gmail.com>
2021-12-01 00:40:28 +0200

    migrations

---
+++ src/contracts/Tether.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+
+contract Tether {
+    string public name = "Mock Tether";
+    string public symbol = "mUSDT";
+    uint public totalSupply = 1000000000000000000000000; // 1 million supply
+    uint public decimals = 18;
+}
